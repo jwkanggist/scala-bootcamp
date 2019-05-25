3942279266b088a5ef99f3fcb13577907f512edc
Jaewook Kang <jwkang10@gmail.com>
2019-05-25 14:38:34 +0900

    [basic-syntax] correct some bug

--- scala-basic-syntax/src/main/scala/Day2/ClassAndObject.scala
+++ scala-basic-syntax/src/main/scala/Day2/ClassAndObject.scala
@@ -19,7 +19,41 @@ package  Day2
   }
  */
 
-class ClassAndObject {
+
+// A companion objects of the class Animal
+object ClassAndObject{
+  def main(args: Array[String]): Unit ={
+    val obj: ClassAndAnimalObject = new ClassAndAnimalObject
+    val extendObj: ClassAndAnimalObjectExcepetion = new ClassAndAnimalObjectExcepetion
+
+    var flag: Boolean = false
+
+    obj.setAnimalName("dog")
+    flag = obj.isAnimalNameSet
+    println("obj.AnimalName=",obj.getAnimalName())
+    println("flag:",flag)
+
+    obj.setAnimalName("noset")
+    flag = obj.isAnimalNameSet
+    println("obj.AnimalName=",obj.getAnimalName())
+    println("flag:",flag)
+
+    extendObj.setAnimalName("dog")
+    extendObj.setAnimalAge(11)
+    extendObj.setAnimalWeightKg(24)
+    flag = extendObj.isAnimalNameSet
+    println("extendObj.AnimalName =", extendObj.getAnimalName())
+    println("extendObj.AnimalAge =", extendObj.getAnimalAge())
+    println("extendObj.AnimalWeightKg=", extendObj.getAnimalWeightKg())
+    println("flag:",flag)
+
+  }
+}
+
+
+
+
+class ClassAndAnimalObject {
 
   var animalName: String= "noset"
   var animalAge:  Int = -1
@@ -55,7 +89,7 @@ class ClassAndObject {
 
  */
 
-class ClassAndObjectExpetion extends ClassAndObject{
+class ClassAndAnimalObjectExcepetion extends ClassAndAnimalObject{
 
   var animalWeightKg = 54
 
@@ -77,7 +111,7 @@ class ClassAndObjectExpetion extends ClassAndObject{
     - 2) an singleton instance object: a single object of a class can exist
     ==> 여기서는 2번으로 사용하고 있음
     ==> 싱글톤 객체로 사용하기 위해서 object 키워드를 통해서 선언한다.
-    ==> 
+    ==>
 
     Format:
 
@@ -87,37 +121,3 @@ class ClassAndObjectExpetion extends ClassAndObject{
       }
 
  */
-
-
-// A companion objects of the class Animal
-object ClassAndObject{
-  def main(args: Array[String]): Unit ={
-    val obj: ClassAndObject = new ClassAndObject
-    val extendObj: ClassAndObjectExpetion = new ClassAndObjectExpetion
-
-    var flag: Boolean = false
-
-    obj.setAnimalName("dog")
-    flag = obj.isAnimalNameSet
-    println("obj.AnimalName=",obj.getAnimalName())
-    println("flag:",flag)
-
-    obj.setAnimalName("noset")
-    flag = obj.isAnimalNameSet
-    println("obj.AnimalName=",obj.getAnimalName())
-    println("flag:",flag)
-
-    extendObj.setAnimalName("dog")
-    extendObj.setAnimalAge(11)
-    extendObj.setAnimalWeightKg(24)
-    flag = extendObj.isAnimalNameSet
-    println("extendObj.AnimalName =", extendObj.getAnimalName())
-    println("extendObj.AnimalAge =", extendObj.getAnimalAge())
-    println("extendObj.AnimalWeightKg=", extendObj.getAnimalWeightKg())
-    println("flag:",flag)
-
-  }
-}
-
-
-
